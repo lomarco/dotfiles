618f0935d2b0ab507f1c30cfe14d34da41bd4676
VorPijakov23 <119614090+lomarco@users.noreply.github.com>
2025-04-26 18:52:35 +0700

    Update build.sh script

--- build.sh
+++ build.sh
@@ -1,24 +1,55 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 
 set -e
 
 bin_scripts="/usr/local/bin/"
 configs_dir="./config_files/"
+script_name="${0##*/}"
+
+error() {
+  echo -e "\e[1;31m$script_name: $1\e[0m" >&2
+}
+
+success() {
+  echo -e "\e[1;32m$script_name: $1\e[0m"
+}
 
 for arg in "$@"; do
-	if [ "$arg" == "-c" ] || [ "$arg" == "--clean" ] || [ "$arg" == "--clear" ]; then
-		sudo rm -rf $configs_dir ./bin
-		break
-	fi
+  if [[ "$arg" == "-c" || "$arg" == "--clean" ]]; then
+    if rm -rf "$configs_dir" ./bin; then
+      success "$arg: Successful"
+    else
+      error "$arg: failed to clean directories"
+      exit 1
+    fi
+  fi
 done
 
+mkdir -p "$configs_dir"
 
-mkdir -p $configs_dir 
+cp -r "$bin_scripts" .
 
-sudo cp -r $bin_scripts . 
-cp -r $HOME/.config/foot/foot.ini $configs_dir
-cp -r $HOME/.config/sway/config $configs_dir
-cp -r $HOME/.config/waybar/ $configs_dir
-cp -r $HOME/.zshrc $configs_dir/zshrc
-cp -r $HOME/.config/nvim/init.lua $configs_dir
-cp -r $HOME/.vimrc $configs_dir/vimrc
+declare -a files=(
+  "$HOME/.config/foot/foot.ini|foot.ini"
+  "$HOME/.config/sway/config|config"
+  "$HOME/.config/waybar/|waybar"
+  "$HOME/.zshrc|zshrc"
+  "$HOME/.config/nvim/init.lua|init.lua"
+  "$HOME/.vimrc|vimrc"
+  "$HOME/.gitconfig|gitconfig"
+)
+
+for file in "${files[@]}"; do
+  src="${file%%|*}"
+  dst="${file#*|}"
+  
+  if [[ -d "$src" ]]; then
+    if ! cp -r "$src" "$configs_dir/$dst"; then
+      error "Failed to copy directory $src"
+    fi
+  else
+    if ! cp "$src" "$configs_dir/$dst"; then
+      error "Failed to copy file $src"
+    fi
+  fi
+done
