ea30c27cf9676521bf44854f6e317176745a1599
lomarco <119614090+lomarco@users.noreply.github.com>
2025-05-06 18:39:13 +0700

    Add git-conflicts.nvim

--- config_files/init.lua
+++ config_files/init.lua
@@ -372,6 +372,21 @@ require('lazy').setup({
     config = function()
     end
   },
+  {
+    "akinsho/git-conflict.nvim",
+    config = function()
+      require('git-conflict').setup({
+        default_mappings = true,
+        default_commands = true,
+        disable_diagnostics = false,
+        list_opener = 'copen',
+        highlights = {
+          incoming = 'DiffAdd',
+          current = 'DiffText',
+        },
+      })
+    end
+  }
 })
 
 ------------ CUSTOM:
