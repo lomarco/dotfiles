e36a751fe13b29cd45a096d0c3a0292153456cf4
lomarco <119614090+lomarco@users.noreply.github.com>
2025-05-20 18:21:23 +0700

    Refactoring plugins settings

--- config_files/init.lua
+++ config_files/init.lua
@@ -36,37 +36,51 @@ vim.opt.fillchars = { eob = " " }
 vim.opt.colorcolumn = "80"
 
 
------------- KEYMAPS:
+------------ BASE KEYMAPS:
 vim.g.mapleader = " "
 
+-- Escape alias
 vim.keymap.set('i', 'jk', '<Esc>', { noremap = true, silent = true })
 vim.keymap.set('i', 'kj', '<Esc>', { noremap = true, silent = true })
 
+-- Auto brackets
 vim.keymap.set('v', '<leader>"', 'c""<Esc>P', { noremap = true, silent = true })
 vim.keymap.set('v', "<leader>'", "c''<Esc>P", { noremap = true, silent = true })
 vim.keymap.set('v', '<leader>9', 'c()<Esc>P', { noremap = true, silent = true })
 vim.keymap.set('v', '<leader>{', 'c{}<Esc>P', { noremap = true, silent = true })
 vim.keymap.set('v', '<leader>[', 'c[]<Esc>P', { noremap = true, silent = true })
 
--- Buffers
+-- Buffers moving
 vim.keymap.set('n', 'gn', ':bnext<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', 'gp', ':bprevious<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', 'gw', ':bdelete<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>be', ':enew<CR>', { noremap = true, silent = true })
 
--- Moving between windows
+-- Windows moving
 vim.keymap.set('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
 vim.keymap.set('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
 vim.keymap.set('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
 vim.keymap.set('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
 
--- Fix terminal work
+-- Fix terminal work (Off ESC and Shift-Space hot-keys)
 vim.keymap.set('n', '<Esc>', ':noh<CR><Esc>', { noremap = true, silent = true })
 vim.keymap.set("t", "<S-Space>", "<Space>", { noremap = true, silent = true })
 
 
 ------------ PLULINS
 require('lazy').setup({
+  -- Caching plugin
+  {
+    "lewis6991/impatient.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("impatient")
+    end,
+  },
+
+
+  -- Menus
   {
     'nvim-tree/nvim-tree.lua',
     vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>", { noremap = true, silent = true }),
@@ -127,6 +141,124 @@ require('lazy').setup({
     end
   },
   {
+    'romgrk/barbar.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('barbar').setup({
+        animation = false
+      })
+    end
+  },
+  {
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    keys = {
+      { '<leader>ff', function() require('telescope.builtin').find_files() end, desc = 'Find Files' },
+      { '<leader>fg', function() require('telescope.builtin').live_grep() end,  desc = 'Live Grep' },
+      { '<leader>fb', function() require('telescope.builtin').buffers() end,    desc = 'Find Buffers' },
+      { '<leader>fh', function() require('telescope.builtin').help_tags() end,  desc = 'Help Tags' },
+      { '<leader>fr', function() require('telescope.builtin').oldfiles() end,   desc = 'Recent Files' },
+    },
+    config = function()
+      local telescope = require('telescope')
+      telescope.setup({
+        defaults = {
+          mappings = {
+            i = {
+              ['<C-u>'] = false,
+              ['<C-d>'] = false,
+            },
+          },
+        },
+      })
+    end
+  },
+  {
+    'folke/todo-comments.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {},
+    keys = {
+      { '<leader>ft', '<cmd>TodoTelescope<cr>', desc = 'Find TODOs' }
+    }
+  },
+  {
+    "preservim/tagbar",
+    cmd = "TagbarToggle",
+    config = function()
+      vim.g.tagbar_width = 50
+      vim.g.tagbar_autofocus = 1
+      vim.g.tagbar_sort = 0
+      vim.g.tagbar_compact = 1
+      vim.g.tagbar_autoclose = 0
+      vim.keymap.set('n', '<leader>t', ':TagbarToggle<CR>', { desc = '[T]agbar [T]oggle' })
+    end,
+    keys = {
+      { '<leader>t', ':TagbarToggle<CR>', desc = '[T]agbar [T]oggle' },
+    }
+  },
+  {
+    "mbbill/undotree",
+    cmd = "UndotreeToggle",
+    config = function()
+    end
+  },
+  {
+    'akinsho/toggleterm.nvim',
+    version = "*",
+    keys = { "<A-h>" },
+    config = function()
+      require("toggleterm").setup({
+        size = 20,
+        open_mapping = [[<A-h>]],
+        direction = "horizontal",
+        start_in_insert = true,
+        persist_size = true,
+        close_on_exit = true,
+      })
+    end
+  },
+  {
+    "folke/trouble.nvim",
+    opts = {},
+    cmd = "Trouble",
+    keys = {
+      {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
+    },
+  },
+
+
+  -- Base text plugins
+  {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     event = 'BufRead',
@@ -136,48 +268,6 @@ require('lazy').setup({
         highlight = { enable = true },
         indent = { enable = true },
         auto_install = true,
-      })
-    end
-  },
-  {
-    'lewis6991/gitsigns.nvim',
-    config = function()
-      require('gitsigns').setup({
-        on_attach = function(bufnr)
-          local gs = package.loaded.gitsigns
-
-          local function map(mode, l, r, opts)
-            opts = opts or {}
-            opts.buffer = bufnr
-            vim.keymap.set(mode, l, r, opts)
-          end
-
-          map('n', ']c', function()
-            if vim.wo.diff then return ']c' end
-            vim.schedule(function() gs.next_hunk() end)
-            return '<Ignore>'
-          end, { expr = true })
-
-          map('n', '[c', function()
-            if vim.wo.diff then return '[c' end
-            vim.schedule(function() gs.prev_hunk() end)
-            return '<Ignore>'
-          end, { expr = true })
-
-          map('n', '<leader>hs', gs.stage_hunk)
-          map('n', '<leader>hr', gs.reset_hunk)
-          map('v', '<leader>hs', function() gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
-          map('v', '<leader>hr', function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
-          map('n', '<leader>hS', gs.stage_buffer)
-          map('n', '<leader>hu', gs.undo_stage_hunk)
-          map('n', '<leader>hR', gs.reset_buffer)
-          map('n', '<leader>hp', gs.preview_hunk)
-          map('n', '<leader>hb', function() gs.blame_line { full = true } end)
-          map('n', '<leader>tb', gs.toggle_current_line_blame)
-          map('n', '<leader>hd', gs.diffthis)
-          map('n', '<leader>hD', function() gs.diffthis('~') end)
-          map('n', '<leader>td', gs.toggle_deleted)
-        end
       })
     end
   },
@@ -252,6 +342,9 @@ require('lazy').setup({
       })
     end
   },
+
+
+  -- Additional text plugins
   {
     'numToStr/Comment.nvim',
     config = function()
@@ -263,98 +356,6 @@ require('lazy').setup({
     config = function()
       require('nvim-autopairs').setup()
     end
-  },
-  {
-    'romgrk/barbar.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      require('barbar').setup({
-        animation = false
-      })
-    end
-  },
-  {
-    'rafi/awesome-vim-colorschemes',
-    -- lazy = false,
-    -- priority = 1000,
-    config = function()
-      vim.cmd.colorscheme('purify')
-      -- purify,vscode,molokai,one,elflord,desert,yellow-moon,challenger_deep,industry,gruvbox,retrobox,nord,onedark,pablo,darkblue,blue,PaperColor
-      vim.opt.background = 'dark'
-
-      -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-      -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    end
-  },
-  {
-    "folke/trouble.nvim",
-    opts = {},
-    cmd = "Trouble",
-    keys = {
-      {
-        "<leader>xx",
-        "<cmd>Trouble diagnostics toggle<cr>",
-        desc = "Diagnostics (Trouble)",
-      },
-      {
-        "<leader>xX",
-        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-        desc = "Buffer Diagnostics (Trouble)",
-      },
-      {
-        "<leader>cs",
-        "<cmd>Trouble symbols toggle focus=false<cr>",
-        desc = "Symbols (Trouble)",
-      },
-      {
-        "<leader>cl",
-        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-        desc = "LSP Definitions / references / ... (Trouble)",
-      },
-      {
-        "<leader>xL",
-        "<cmd>Trouble loclist toggle<cr>",
-        desc = "Location List (Trouble)",
-      },
-      {
-        "<leader>xQ",
-        "<cmd>Trouble qflist toggle<cr>",
-        desc = "Quickfix List (Trouble)",
-      },
-    },
-  },
-  {
-    'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    keys = {
-      { '<leader>ff', function() require('telescope.builtin').find_files() end, desc = 'Find Files' },
-      { '<leader>fg', function() require('telescope.builtin').live_grep() end,  desc = 'Live Grep' },
-      { '<leader>fb', function() require('telescope.builtin').buffers() end,    desc = 'Find Buffers' },
-      { '<leader>fh', function() require('telescope.builtin').help_tags() end,  desc = 'Help Tags' },
-      { '<leader>fr', function() require('telescope.builtin').oldfiles() end,   desc = 'Recent Files' },
-    },
-    config = function()
-      local telescope = require('telescope')
-      telescope.setup({
-        defaults = {
-          mappings = {
-            i = {
-              ['<C-u>'] = false,
-              ['<C-d>'] = false,
-            },
-          },
-        },
-      })
-    end
-  },
-  {
-    'folke/todo-comments.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    opts = {},
-    keys = {
-      { '<leader>ft', '<cmd>TodoTelescope<cr>', desc = 'Find TODOs' }
-    }
   },
   {
     "folke/flash.nvim",
@@ -377,55 +378,71 @@ require('lazy').setup({
       vim.keymap.del({ "n", "o", "x" }, ",")
     end,
   },
+
+
+  -- Git
   {
-    "preservim/tagbar",
-    cmd = "TagbarToggle",
+    'lewis6991/gitsigns.nvim',
     config = function()
-      vim.g.tagbar_width = 50
-      vim.g.tagbar_autofocus = 1
-      vim.g.tagbar_sort = 0
-      vim.g.tagbar_compact = 1
-      vim.g.tagbar_autoclose = 0
-      vim.keymap.set('n', '<leader>t', ':TagbarToggle<CR>', { desc = '[T]agbar [T]oggle' })
-    end,
-    keys = {
-      { '<leader>t', ':TagbarToggle<CR>', desc = '[T]agbar [T]oggle' },
-    }
-  },
-  {
-    "mofiqul/vscode.nvim",
-    config = function()
-    end
-  },
-  {
-    "mbbill/undotree",
-    cmd = "UndotreeToggle",
-    config = function()
-    end
-  },
-  {
-    'akinsho/toggleterm.nvim',
-    version = "*",
-    keys = { "<A-h>" },
-    config = function()
-      require("toggleterm").setup({
-        size = 20,
-        open_mapping = [[<A-h>]],
-        direction = "horizontal",
-        start_in_insert = true,
-        persist_size = true,
-        close_on_exit = true,
+      require('gitsigns').setup({
+        on_attach = function(bufnr)
+          local gs = package.loaded.gitsigns
+
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+          end
+
+          map('n', ']c', function()
+            if vim.wo.diff then return ']c' end
+            vim.schedule(function() gs.next_hunk() end)
+            return '<Ignore>'
+          end, { expr = true })
+
+          map('n', '[c', function()
+            if vim.wo.diff then return '[c' end
+            vim.schedule(function() gs.prev_hunk() end)
+            return '<Ignore>'
+          end, { expr = true })
+
+          map('n', '<leader>hs', gs.stage_hunk)
+          map('n', '<leader>hr', gs.reset_hunk)
+          map('v', '<leader>hs', function() gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+          map('v', '<leader>hr', function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+          map('n', '<leader>hS', gs.stage_buffer)
+          map('n', '<leader>hu', gs.undo_stage_hunk)
+          map('n', '<leader>hR', gs.reset_buffer)
+          map('n', '<leader>hp', gs.preview_hunk)
+          map('n', '<leader>hb', function() gs.blame_line { full = true } end)
+          map('n', '<leader>tb', gs.toggle_current_line_blame)
+          map('n', '<leader>hd', gs.diffthis)
+          map('n', '<leader>hD', function() gs.diffthis('~') end)
+          map('n', '<leader>td', gs.toggle_deleted)
+        end
       })
     end
   },
+
+
+  -- Themes
   {
-    "lewis6991/impatient.nvim",
-    lazy = false,
-    priority = 1000,
+    'rafi/awesome-vim-colorschemes',
     config = function()
-      require("impatient")
-    end,
-  }
+      vim.cmd.colorscheme('purify')
+      -- purify,vscode,molokai,one,elflord,desert,yellow-moon,challenger_deep,industry,gruvbox,retrobox,nord,onedark,pablo,darkblue,blue,PaperColor
+      vim.opt.background = 'dark'
+
+      -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+      -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    end
+  },
+  {
+    "mofiqul/vscode.nvim",
+    lazy = true,
+    config = function()
+    end
+  },
 })
 
 
@@ -440,7 +457,7 @@ vim.api.nvim_create_autocmd('FileType', {
 })
 
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'c,cpp',
+  pattern = 'c',
   callback = function()
     -- vim.keymap.set('n', '<c-b>', ':!make && make clean:r<cr>', { noremap = true, silent = true })
     vim.keymap.set('n', '<c-b>', ':!clang % && ./a.out<cr>', { noremap = true, silent = true })
