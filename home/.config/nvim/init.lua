f439a8678b427ab8beac806ad26cb3925b639626
lomarco <119614090+lomarco@users.noreply.github.com>
2026-05-16 00:31:19 +0700

    Delete excess var

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -285,9 +285,9 @@ require('lazy').setup({
           vim.notify("Could not determine current branch", vim.log.levels.ERROR)
           return
         end
-        local cmd = string.format("Git push origin %s", branch)
-        vim.cmd(cmd)
-      end, { noremap = true, silent = true, desc = "Git push current branch" })
+        vim.cmd(string.format("Git push origin %s", branch))
+        vim.notify("Pushed branch: " .. branch, vim.log.levels.INFO)
+      end, { noremap = true, desc = "Git push current branch" })
       vim.g.fugitive_legacy_commands = 0
     end,
   },
